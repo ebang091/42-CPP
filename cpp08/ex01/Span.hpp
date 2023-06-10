8c9896dc931e04938bbb347fc07e59a9bb4ce974
은정 방 <108053947+ebang091@users.noreply.github.com>
2023-06-11 00:06:54 +0900

    Create Span.hpp

---
+++ cpp08/ex01/Span.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#ifndef SPAN_HPP
+# define SPAN_HPP
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class Span
+{
+
+private:
+	std::vector<int> vec;
+	unsigned int idx;
+	unsigned int max_len;
+	bool isSorted;
+
+public:
+	Span(unsigned int N);
+	Span(const Span &copy);
+	Span& operator=(const Span &copy);
+	~Span();
+
+	void addNumber(int n);
+
+	long long  shortestSpan();
+	long long longestSpan();
+	unsigned int size() const ;
+	int getNum(unsigned int idx) const;
+
+
+};
+
+#endif
