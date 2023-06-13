e8962c557d5bb25d1692aa137b84de624175198f
ebang <dskhe8@gmail.com>
2023-06-13 14:25:43 +0900

    fix : vector iterator rather than auto

--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -4,14 +4,14 @@ int main() {
     std::vector<int> numbers = {1, 2, 3, 4, 5};
 
     try {
-        auto it = easyfind(numbers, 3);
+        std::vector::iterator it = easyfind(numbers, 3);
         std::cout << "Value found at index: " << std::distance(numbers.begin(), it) << std::endl;
     } catch (const std::exception& e) {
         std::cout << e.what() << std::endl;
     }
     
     try {
-        auto it = easyfind(numbers, 6);
+        std::vector::iterator it = easyfind(numbers, 6);
         std::cout << "Value found at index: " << std::distance(numbers.begin(), it) << std::endl;
     } catch (const std::exception& e) {
         std::cout << e.what() << std::endl;
