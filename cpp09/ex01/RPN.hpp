2433cfd827c5ea370a0a18fd060db7d9f9f1c689
ebang <dskhe8@gmail.com>
2023-06-17 02:44:56 +0900

    start ex01

---
+++ cpp09/ex01/RPN.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef RPN_HPP
+# define RPN_HPP
+
+class RPN
+{
+private:
+        /* data */
+public:
+        RPN();
+        RPN(const RPN& copy);
+        RPN& operator=(const RPN& copy);
+        ~RPN();
+
+};
+
+#endif // RPN_HPP
