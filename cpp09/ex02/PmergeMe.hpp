8acc3a2136a497a645f11fbf6b60568798786c57
ebang091 <dskhe8@gmail.com>
2023-06-23 01:45:17 +0900

    start ex00

---
+++ cpp09/ex02/PmergeMe.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#ifndef PMERGEME_HPP
+# define PMERGEME_HPP
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <string>
+typedef struct {
+	int first;
+	int second;
+}node;
+
+class PmergeMe {
+private:
+
+
+public:
+
+
+};
+
+#endif
